10c5ba30a001a2f15b233502c4d8426ff47eda66
ankit kumar gupta <ankit.gupta@impactanalytics.co>
2019-01-18 17:16:27 +0530

    sending response with text

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers)  // / GET {...}
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html>');
+    res.write('ankit');
+    res.write('</html>');
     // process.exist() //exist the server
 })
 
