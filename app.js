0ea90eaf83604b18bac13a20fa49ee8c4a60afb6
ankit kumar gupta <ankit.gupta@impactanalytics.co>
2019-01-18 18:44:27 +0530

    sending html form from server

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers)  // / GET {...}
+    if (req.url === '/') { //first time control will come here as /
+        res.setHeader('Content-type', 'text/html');
+        res.write('<html>');   //here /message will append in localhost:8080/
+        res.write('<form action="/message" method="post"><input type="text" name="name"><button type="submit">Submit</button></form>');
+        res.write('</html>');
+        return res.end();
+    } 
+    //since after submitting form, url change to hhtp://localhost:8080/message/ control will come here
     res.setHeader('Content-type', 'text/html');
-    res.write('<html>');
-    res.write('ankit');
-    res.write('</html>');
+        res.write('<html>anitfdfsd');   //here /message will append in localhost:8080/
+        res.write('</html>');
+    res.end();   //ending the request
     // process.exist() //exist the server
 })
 
