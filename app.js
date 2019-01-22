3d0c97648552a84eee8059482dcff07a9c01b61e
ankit kumar gupta <ankit.gupta@impactanalytics.co>
2019-01-22 16:24:46 +0530

    handling routes in express

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use('/handle-message', (req, res, next) => {
     console.log('in one middleware');
+    res.send(" I am in first one");
       //if we are not using next() then it will not execute 2nd middleware
-    next();
 })
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('in 2nd middleware');
-    res.send('<html>ewewew</html>'); //by default express add conteext-type: texrt/html
+    res.send('<html>2nd one</html>'); //by default express add conteext-type: texrt/html
     // res.send('<html>ewewew</html>')  //still page not loaded as we have not returned any response
 })
 
