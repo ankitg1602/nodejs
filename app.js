bb747f2a99ad5ce25fe6d436536c5995833b50fa
ankit kumar gupta <ankit.gupta@impactanalytics.co>
2019-01-22 15:31:44 +0530

    how middleware works

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('in one middleware');   //if we are not using next() then it will not execute 2nd middleware
+    console.log('in one middleware');
+      //if we are not using next() then it will not execute 2nd middleware
     next();
 })
 
 app.use((req, res, next) => {
-    console.log('in 2nd middleware');  //still page not loaded as we have not returned any response
+    console.log('in 2nd middleware');
+    res.send('<html>ewewew</html>'); 
+    // res.send('<html>ewewew</html>')  //still page not loaded as we have not returned any response
 })
 
 const server = http.createServer(app);
