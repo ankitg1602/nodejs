067d05018a9c9a39c5005f3d9b6d8327f748b7d1
ankit kumar gupta <ankit.gupta@impactanalytics.co>
2019-01-22 15:59:07 +0530

    removed http and used express for listener

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+// const http = require('http');
 
 const express = require('express');
 
@@ -12,10 +12,12 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     console.log('in 2nd middleware');
-    res.send('<html>ewewew</html>'); 
+    res.send('<html>ewewew</html>'); //by default express add conteext-type: texrt/html
     // res.send('<html>ewewew</html>')  //still page not loaded as we have not returned any response
 })
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(3000);
+app.listen(3000);  //by using expressjs, we can listen without using http
+
+// server.listen(3000);
