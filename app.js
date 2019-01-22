280fd418077367f2674f7cb21c803405f310d17b
ankit kumar gupta <ankit.gupta@impactanalytics.co>
2019-01-22 19:32:22 +0530

    page not found handle

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const shopRoutes = require('./routes/shop')
 
 const app = express();
 
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use(adminRoutes);
 
 app.use(shopRoutes);
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use((req, res, next) => {
+    res.status(404).send('<p>Page not found</p>');   //here default route is / and use method will compare starting from /
+})
 
 app.listen(3000);
