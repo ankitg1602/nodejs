438655787e14c89c8d0107d3f9f286148fd674a0
ankit kumar gupta <ankit.gupta@impactanalytics.co>
2019-01-22 15:17:13 +0530

    adding middleware

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('in one middleware');   //if we are not using next() then it will not execute 2nd middleware
+    next();
+})
+
+app.use((req, res, next) => {
+    console.log('in 2nd middleware');  //still page not loaded as we have not returned any response
+})
+
 const server = http.createServer(app);
 
 server.listen(3000);
