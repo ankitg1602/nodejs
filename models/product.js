5cb319a7ed048a537149aa93db17ece4d3e4e765
ankit kumar gupta <ankit.gupta@impactanalytics.co>
2019-01-24 18:25:49 +0530

    defining model in sequelize

--- models/product.js
+++ models/product.js
@@ -1,30 +1,19 @@
-const db = require('../util/database');
+const Sequelize = require('sequelize');
 
-const Cart = require('./cart');
+const sequelize = require('../util/database');
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const Claim = sequelize.define('claim', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  expense_details: Sequelize.STRING,
+  bill_available: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
   }
+});
 
-  save() {
-    return db.execute(
-      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
-
-  static deleteById(id) {}
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
-};
+module.exports = Claim
