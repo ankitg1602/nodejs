7fb934e71e0348d9db22e0c9afd90ae9159d8664
ankit kumar gupta <ankit.gupta@impactanalytics.co>
2019-01-24 13:12:32 +0530

    removed database credentail

--- util/database.js
+++ util/database.js
@@ -1,11 +1,7 @@
 const pg = require('pg');
 
 const pool = {
-    username: 'irt_test',
-    password: '@owering@eople@rogress',
-    host: '35.232.25.11',
-    database: 'ankit',
-    port: 5432
+    
 }
 
 const client = new pg.Client(`postgres://${pool.username}:${pool.password}@${pool.host}:${pool.port}/${pool.database}`);
