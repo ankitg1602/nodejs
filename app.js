229360d9596a1b95e9a3b2b661ab826a96dee6ac
ankit kumar gupta <ankit.gupta@impactanalytics.co>
2019-01-24 13:12:00 +0530

    connect out app to postgresql

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop')
+
+const db = require('./util/database');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);  //it will match url with /admin/add-product
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).send('<p>Page not found</p>');   //here default route is / and use method will compare starting from /
-})
+app.use(errorController.get404);
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,24 @@
+const path = require('path');
+
 const express = require('express');
+
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-// /admin/add-product   => GET
-router.get('/add-product', (req, res, next) => {
-  res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-  
-// /admin/add-product  => POST
-router.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
+
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
